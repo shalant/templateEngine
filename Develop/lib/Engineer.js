5c58d89b03a28bca7b385e0ef9502b72fc332312
Doug Rosenberg <doug.rosenberg@gmail.com>
2020-09-10 16:20:23 -0500

    liquidated node modules

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,7 +1,16 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
+const Employee = require("./Engineer");
+
 
 //inherit thingy
+class Engineer extends Employee {
+    constructor (id, officeNum, whatevs){
+        super(id, officeNum)
+        this.whatevs = whatevs
+    }
+}
+
 
 //from readme:
 this.gitHub = gitHub
